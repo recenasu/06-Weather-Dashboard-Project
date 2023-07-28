49f209c22520047b5f351e1da955a0456bebaaf9
Domitius <133902698+recenasu@users.noreply.github.com>
2023-07-28 15:24:14 -0700

    added variables for all weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,10 @@
 var APIKeyOW = "ca5c36fad955c2432c0ac7166c71cfdd";
 // Variables to store location info
 var city;
-var state;
-var country;
 
-city = "Phoenix";
+city = "San Francisco";
 
-// Obtains the current weather for the city from Open Weather
+// This function obtains the current weather for the city from Open Weather
 function getCurrentWxApi() {
 
 // variable for the current weather api URL. Query for city weather in imperial units.
@@ -18,86 +16,95 @@ fetch(currentWxUrl)
 .then(function (response) {
     return response.json();
 })
-.then(function (data) {
-    console.log(data); //full  array for dev reference
-    console.log(data.name); //city name
-    console.log(data.wind.speed); // current wind
-    console.log(data.main.temp); // current temp
-    console.log(data.main.temp_min); //today's min temp
-    console.log(data.main.temp_max); //today's max temp
-    console.log(data.main.humidity); //current humidity
-    console.log(data.weather); //this is for the icon
+.then(function (currentData) {
+    
+    // Current day weather data set to variables and displayed in the console 
+    var currentDayWx = currentData;
+    var currentDayCity = currentData.name;
+    var currentDayWinds = currentData.wind.speed;
+    var currentDayTemp = currentData.main.temp;
+    var currentDayTempMin = currentData.main.temp_min;
+    var currentDayTempMax = currentData.main.temp_max;    var currentDayHumid = currentData.main.humidity;
+    var currentDayIcon = "https://openweathermap.org/img/wn/"+ currentData.weather[0].icon +"@2x.png";
+    console.log(currentDayWx);
+    console.log(currentDayCity);
+    console.log(currentDayWinds);
+    console.log(currentDayTemp);
+    console.log(currentDayTempMin);
+    console.log(currentDayTempMax);
+    console.log(currentDayHumid);
+    console.log(currentDayIcon);
 })
 }
 
-// Obtains the 5-day forecast for the city from Open Weather.
+// This function obtains the 5-day forecast for the city from Open Weather.
 function get5DayForecastApi() {
 
-    // variable for the current weather api URL. Query for city weather in imperial units.
-    var currentWxUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+ city +"&appid="+ APIKeyOW +"&units=imperial";
+    // variable for the 5-day forecast api URL. Query for city weather in imperial units.
+    var fiveDayWxUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+ city +"&appid="+ APIKeyOW +"&units=imperial";
     
     // fetch method to the current weather api URL
-    fetch(currentWxUrl)
+    fetch(fiveDayWxUrl)
     .then(function (response) {
         return response.json();
     })
-    .then(function (data) {
-        console.log(data); //full array for dev reference
+    .then(function (fiveDayData) {
+        console.log(fiveDayData); //full array for dev reference
         
-        // Day 1 forecast data set to variables and displayed in the console 
-        var day1Wx = data.list[7]; 
-        var day1WxTemp = data.list[7].main.temp;
-        var day1WxHumid = data.list[7].main.humidity;
-        var day1WxWinds = data.list[7].wind.speed;
-        var day1WxIcon = data.list[7].weather;
+        // Day 1 forecast fiveDayData set to variables and displayed in the console 
+        var day1Wx = fiveDayData.list[7]; 
+        var day1WxTemp = fiveDayData.list[7].main.temp;
+        var day1WxHumid = fiveDayData.list[7].main.humidity;
+        var day1WxWinds = fiveDayData.list[7].wind.speed;
+        var day1WxIcon = "https://openweathermap.org/img/wn/"+ fiveDayData.list[7].weather[0].icon +"@2x.png";
         console.log(day1Wx); 
         console.log(day1WxTemp);
         console.log(day1WxHumid);
         console.log(day1WxWinds);
         console.log(day1WxIcon);
 
-        // Day 2 forecast data set to variables and displayed in the console 
-        var day2Wx = data.list[15]; 
-        var day2WxTemp = data.list[15].main.temp;
-        var day2WxHumid = data.list[15].main.humidity;
-        var day2WxWinds = data.list[15].wind.speed;
-        var day2WxIcon = data.list[15].weather;
+        // Day 2 forecast fiveDayData set to variables and displayed in the console 
+        var day2Wx = fiveDayData.list[15]; 
+        var day2WxTemp = fiveDayData.list[15].main.temp;
+        var day2WxHumid = fiveDayData.list[15].main.humidity;
+        var day2WxWinds = fiveDayData.list[15].wind.speed;
+        var day2WxIcon = "https://openweathermap.org/img/wn/"+ fiveDayData.list[15].weather[0].icon +"@2x.png";;
         console.log(day2Wx); 
         console.log(day2WxTemp);
         console.log(day2WxHumid);
         console.log(day2WxWinds);
         console.log(day2WxIcon);
         
-        // Day 3 forecast data set to variables and displayed in the console 
-        var day3Wx = data.list[23]; 
-        var day3WxTemp = data.list[23].main.temp;
-        var day3WxHumid = data.list[23].main.humidity;
-        var day3WxWinds = data.list[23].wind.speed;
-        var day3WxIcon = data.list[23].weather;
+        // Day 3 forecast fiveDayData set to variables and displayed in the console 
+        var day3Wx = fiveDayData.list[23]; 
+        var day3WxTemp = fiveDayData.list[23].main.temp;
+        var day3WxHumid = fiveDayData.list[23].main.humidity;
+        var day3WxWinds = fiveDayData.list[23].wind.speed;
+        var day3WxIcon = "https://openweathermap.org/img/wn/"+ fiveDayData.list[23].weather[0].icon +"@2x.png";;
         console.log(day3Wx); 
         console.log(day3WxTemp);
         console.log(day3WxHumid);
         console.log(day3WxWinds);
         console.log(day3WxIcon);
 
-        // Day 4 forecast data set to variables and displayed in the console 
-        var day4Wx = data.list[31]; 
-        var day4WxTemp = data.list[31].main.temp;
-        var day4WxHumid = data.list[31].main.humidity;
-        var day4WxWinds = data.list[31].wind.speed;
-        var day4WxIcon = data.list[31].weather;
+        // Day 4 forecast fiveDayData set to variables and displayed in the console 
+        var day4Wx = fiveDayData.list[31]; 
+        var day4WxTemp = fiveDayData.list[31].main.temp;
+        var day4WxHumid = fiveDayData.list[31].main.humidity;
+        var day4WxWinds = fiveDayData.list[31].wind.speed;
+        var day4WxIcon = "https://openweathermap.org/img/wn/"+ fiveDayData.list[31].weather[0].icon +"@2x.png";;
         console.log(day4Wx); 
         console.log(day4WxTemp);
         console.log(day4WxHumid);
         console.log(day4WxWinds);
         console.log(day4WxIcon);
 
-        // Day 5 forecast data set to variables and displayed in the console 
-        var day5Wx = data.list[39]; 
-        var day5WxTemp = data.list[39].main.temp;
-        var day5WxHumid = data.list[39].main.humidity;
-        var day5WxWinds = data.list[39].wind.speed;
-        var day5WxIcon = data.list[39].weather;
+        // Day 5 forecast fiveDayData set to variables and displayed in the console 
+        var day5Wx = fiveDayData.list[39]; 
+        var day5WxTemp = fiveDayData.list[39].main.temp;
+        var day5WxHumid = fiveDayData.list[39].main.humidity;
+        var day5WxWinds = fiveDayData.list[39].wind.speed;
+        var day5WxIcon = "https://openweathermap.org/img/wn/"+ fiveDayData.list[39].weather[0].icon +"@2x.png";;
         console.log(day5Wx); 
         console.log(day5WxTemp);
         console.log(day5WxHumid);
